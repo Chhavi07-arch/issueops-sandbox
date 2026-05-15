c94d4cd89cdd212a6068abc774bdc6a72ea42ae4
Chhavi07-arch <chhavi.24bcs10201@sst.scaler.com>
2026-05-15 17:17:24 +0530

    test file updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-def process_input(user_input):
-    return user_input.strip().lower()
+def is_adult(age):
+    return age > 18
 
 
 if __name__ == "__main__":
-    print(process_input(None))
+    print(is_adult(18))
